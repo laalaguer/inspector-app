8e92b89bf9766bd0fbb00e2796f711ba9938ebad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-25 09:46:01 +0000

    Deploy inspector.vecha.in to github.com/vechain/inspector-app.git:gh-pages

---
+++ precache-manifest.465a9b3eea23e6fa5d152d09ee32bf82.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "/fonts/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/fonts/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "a4e0236cd92d8121bd91",
+    "url": "/css/chunk-vendors.68141692.css"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "/fonts/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "/fonts/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "/fonts/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "/fonts/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "/fonts/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "/fonts/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "8401ad6f5ca374dab388",
+    "url": "/js/app.90d6049f.js"
+  },
+  {
+    "revision": "a4e0236cd92d8121bd91",
+    "url": "/js/chunk-vendors.9dfb8afb.js"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "/fonts/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "/fonts/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "/fonts/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "/fonts/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "42f9fd6acee87559ac0d6a33488db65e",
+    "url": "/img/fa-brands-400.42f9fd6a.svg"
+  },
+  {
+    "revision": "0c41971339b9fc5b1cefb0abad1e2e69",
+    "url": "/img/fa-regular-400.0c419713.svg"
+  },
+  {
+    "revision": "4478b4d7022cad174e4c04246fe622ef",
+    "url": "/img/fa-solid-900.4478b4d7.svg"
+  },
+  {
+    "revision": "54acda965c6319c87ef70e383fec8c84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8401ad6f5ca374dab388",
+    "url": "/css/app.4d77606e.css"
+  }
+];
